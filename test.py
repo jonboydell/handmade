3579c831d814ee842eaa4d70c1ba73399b932fc1
jonboydell <jonboydell@hotmail.com>
2023-07-23 15:53:11 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
+import json
 
+def load_config():
+    conditions = []
+    values = []
+    mapping = json.load('m1.json')
+    for k in mapping.keys():
+        search_terms = mapping[k]
+        for s in search_terms:
+            conditions.append(s)
+            values.append(k)
+    return conditions, values
+      
+      
+  
